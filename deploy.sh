2835240474cc3bee173739fb777cc82d9a4f15fd
Kapil Neupane <kplmanu11@outlook.com>
2021-01-22 23:11:45 +0545

    some changes

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:kplmanu11/dkinternationalFinal.git master:gh-pages
+git push -f git@github.com:kplmanu11/dkinternational.git master:gh-pages
 cd -
