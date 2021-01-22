5baf7e3b70d95e779fc2606bfe203b5f59808c8d
Kapil Neupane <kplmanu11@outlook.com>
2021-01-22 23:16:54 +0545

    some changes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dkinternational.github.io/'
+    ? '/dkinternational/'
     : '/',
   devServer: {
     disableHostCheck: true,
